e55da37f5018e7a330448d2b20e71f0eac245c30
RussianLord <dj_volume@mail.ru>
2022-12-10 15:50:30 +0300

    третье задание

--- HW6.py
+++ HW6.py
@@ -1,3 +1,5 @@
+# Задача про планеты
+
 # import math
 # orbits = [(1, 3), (2.5, 10), (7,2), (6, 6), (4, 3)]
 # def find_farthest_orbit(list_of_orbit):  
@@ -16,14 +18,26 @@
 #     print(f'Планета с орбитой {maxOrbit} является самой дальней и площадь орбиты равна {maxValue}')
 # find_farthest_orbit(orbits)
 
+# Задача про одинаковые/не одинаковые числа из списка
 
-values = [0, 2, 10, 6]
+# values = [0, 2, 10, 6]
 
-def same_by(func, val):
-    return len(set(map(func, val))) == 1 if val else True
+# def same_by(func, val):
+#     return len(set(map(func, val))) == 1 if val else True
 
     
-if same_by(lambda x: x % 2, values):
-    print('same')
-else:
-    print('different')
+# if same_by(lambda x: x % 2, values):
+#     print('same')
+# else:
+#     print('different')
+
+# Задача про табрицу
+
+# def print_operation_table(operation, num_rows=9, num_columns=9):
+#     for i in range(1, num_rows + 1):
+#         answer = []
+#         for j in range(1, num_columns + 1):
+#             answer.append(str(operation(i, j)))
+#         print("\t".join(answer))
+
+# print_operation_table(lambda x, y: x * y)
