dd12d308500db914d8fc7682d0cb5acb0f9cd148
RussianLord <dj_volume@mail.ru>
2022-12-10 15:44:32 +0300

    сделалй второе задание

--- HW6.py
+++ HW6.py
@@ -1,17 +1,29 @@
-import math
-orbits = [(1, 3), (2.5, 10), (7,2), (6, 6), (4, 3)]
-def find_farthest_orbit(list_of_orbit):  
-    maxValue = 0
-    maxOrbit = ''
-    for i in range(len(orbits)):
-        a = orbits[i][0]
-        b = orbits[i][1]
-        print(a, b)
-        S = math.pi * a * b
-        print(S)
-        if maxValue < S:
-            maxValue = S
-            maxOrbit = f'{a},{b}'
+# import math
+# orbits = [(1, 3), (2.5, 10), (7,2), (6, 6), (4, 3)]
+# def find_farthest_orbit(list_of_orbit):  
+#     maxValue = 0
+#     maxOrbit = ''
+#     for i in range(len(orbits)):
+#         a = orbits[i][0]
+#         b = orbits[i][1]
+#         print(a, b)
+#         S = math.pi * a * b
+#         print(S)
+#         if maxValue < S:
+#             maxValue = S
+#             maxOrbit = f'{a},{b}'
         
-    print(f'Планета с орбитой {maxOrbit} является самой дальней и площадь орбиты равна {maxValue}')
-find_farthest_orbit(orbits)
+#     print(f'Планета с орбитой {maxOrbit} является самой дальней и площадь орбиты равна {maxValue}')
+# find_farthest_orbit(orbits)
+
+
+values = [0, 2, 10, 6]
+
+def same_by(func, val):
+    return len(set(map(func, val))) == 1 if val else True
+
+    
+if same_by(lambda x: x % 2, values):
+    print('same')
+else:
+    print('different')
